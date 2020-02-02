c28c3d0a84bdca7173a8a14dbd8c589ac822d72e
xieyezi <1435398529@qq.com>
2020-02-02 18:44:40 +0800

    修复微信白屏bug

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'babel-polyfill'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
